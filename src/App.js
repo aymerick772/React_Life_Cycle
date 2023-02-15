d94b96f38c0011aa228b44d82bee135690beed94
Aymerick772 <Aymerick772@gmail.com>
2023-02-15 21:14:45 +0400

    laisser toujours le shouldComponentUpdate a true pour l'instant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component} from "react"
 import './App.css';
-import MyComponent from './components/MyComponent';
 
+import ParentComponent from "./components/ParentComponent";
 
 class App extends Component{
   
   state = {
-    age: 27,
+    
   }
 
-  addOneYear =() =>{
-    this.setState((prevState) => ({
-      age: prevState.age + 1
-    }))
-  }
+
 
   render() {
     return (
       <div className="App">
-        <MyComponent age={this.state.age}></MyComponent>
-        <button onClick={this.addOneYear}>Changer le props</button>
+        <ParentComponent/>
       </div>
     );
   }
